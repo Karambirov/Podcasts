0b57676690da004c85fe1c7db00b16a8842286bd
Eugene Karambirov <eugenekarambirov@gmail.com>
2020-07-03 17:38:01 +0300

    Create Xcode project

---
+++ Podcasts/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchListView.swift
+//  Podcasts
+//
+//  Created by Eugene Karambirov on 03.07.2020.
+//
+
+import SwiftUI
+
+struct SearchListView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct SearchListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchListView()
+    }
+}
